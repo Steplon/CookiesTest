fe1b3861df06cd3ff0de9e8afa319060904caae8
Steplon <43827083+Steplon@users.noreply.github.com>
2019-04-12 10:20:41 +0100

    Cookie Testing

--- CookiesTest/Pages/Index.cshtml.cs
+++ CookiesTest/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,28 @@ namespace CookiesTest.Pages
 {
     public class IndexModel : PageModel
     {
+
+        public string Message { get; set; }
+
+
         public void OnGet()
         {
+           
 
+            if (Request.Cookies["NiallCookie"] != null)
+            {
+                Message = Request.Cookies["NiallCookie"];
+            }
+            else
+            {
+                Message = "Hello newcomer!";
+            }
+
+            CookieOptions options = new CookieOptions();
+
+            options.Expires = DateTime.Now.AddHours(1);
+
+            Response.Cookies.Append("NiallCookie", "Welcome back", options);
         }
     }
 }
